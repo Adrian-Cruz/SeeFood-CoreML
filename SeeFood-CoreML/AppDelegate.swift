3f9bbcec7946a6d3a13a42a5b0222888c8058ce2
Adrian <adrian@piso13.com>
2019-07-19 17:00:38 -0500

    Added project to repository
    Added project to repository

--- SeeFood-CoreML/AppDelegate.swift
+++ SeeFood-CoreML/AppDelegate.swift
@@ -2,8 +2,8 @@
 //  AppDelegate.swift
 //  SeeFood-CoreML
 //
-//  Created by Angela Yu on 27/06/2017.
-//  Copyright © 2017 Angela Yu. All rights reserved.
+//  Created by Adrian on 27/06/2017.
+//  Copyright © 2017 Adrian. All rights reserved.
 //
 
 import UIKit
